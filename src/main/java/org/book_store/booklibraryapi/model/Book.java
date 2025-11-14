ce2cd6e354ff1cfc64400ea638c3fdba0928134f
kuldeep yadav <ky5167520@gmail.com>
2025-11-14 15:00:36 +0530

    Initial commit

---
+++ src/main/java/org/book_store/booklibraryapi/model/Book.java
@@ -0,0 +1,22 @@
+package org.book_store.booklibraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Generated("101")
+    private Long bookId;
+    private String title;
+    private String isbn;
+    private int publishedYear;
+}
---
+++ src/test/java/org/book_store/booklibraryapi/BookLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.book_store.booklibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
