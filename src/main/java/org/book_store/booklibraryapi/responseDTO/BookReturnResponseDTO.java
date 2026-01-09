454c0c3715699abc8731d2f234fc1ab0d867025a
kuldeep yadav <ky5167520@gmail.com>
2026-01-09 19:33:29 +0530

    Make Book Return DTO class

--- src/main/java/org/book_store/booklibraryapi/responseDTO/BookReturnResponseDTO.java
+++ src/main/java/org/book_store/booklibraryapi/responseDTO/BookReturnResponseDTO.java
@@ -1,4 +1,13 @@
 package org.book_store.booklibraryapi.responseDTO;
 
+import java.time.LocalDate;
+
 public class BookReturnResponseDTO {
+
+    private Long BorrowRecordId;
+    private String bookName;
+    private String memberName;
+    private LocalDate borrowDate;
+    private LocalDate returnedDate;
+
 }
